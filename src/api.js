ba79c5fbe98bc63b2bba2d8d1aab2ff769a859cf
Vipin Hegde <vipinhegde19@gmail.com>
2026-06-29 11:55:55 +0530

    Update backend URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://applysmart-1-jhr4.onrender.com'
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://applysmart-u6b7.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL.trim()
